59c862e90dc7f6b56c4ec13f75bec08e4b3463d4
Ramil Deocariza <ramildeocariza009@gmail.com>
2026-03-30 11:40:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2915700153-cc2ad",
+  "appId": "1:483905102843:web:05ef8ae94cc37b31d7215e",
+  "apiKey": "AIzaSyBViUfnglSR58afwERD33IrXUVwKJA_UrE",
+  "authDomain": "studio-2915700153-cc2ad.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "483905102843"
+};
